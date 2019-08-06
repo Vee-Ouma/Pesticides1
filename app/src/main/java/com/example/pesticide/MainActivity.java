2e6f7bc8ab45d9881c98d7789b70b4cce2a85439
mckymse <hemhemanth411819.hk@gmail.com>
2019-08-06 18:19:14 +0530

    first update

--- app/src/main/java/com/example/pesticide/MainActivity.java
+++ app/src/main/java/com/example/pesticide/MainActivity.java
@@ -15,14 +15,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String select;
+    String seed,vegetative,flower,fruit,harvest;
     Spinner mSpinner;
     ImageView imageView;
     TextView textView;
     Button button;
     String stage_name;
     String[] mOptions = {"-----SELECT-----","COTTON", "RICE-P", "RICE-K", "SUGARCANE","SUNFLOWER-RAINFED","SUNFLOWER-IRRIGATED"};
-    String[] selectoptions={"STAGES FOR COTTON","STAGES FOR RICE-P","STAGES FOR RICE-K","STAGES FOR SUGARCANE","STAGES FOR SUNFLOWER-RAINFED","STAGED FOR SUNFLOWER-IRRIGATED"};
+    String[] plant_stage={"Pest attack may occur during seedling stage.Click to know more","Pest attack may occur during vegetative stage.Click to know more",
+            "Pest attack may occur during flowering stage.Click to know more","Pest attack may occur during fruiting stage.Click to know more",
+            "Pest attack may occur during harvesting stage.Click to know more"};
+
+
+    String[] select_options={"STAGES FOR COTTON","STAGES FOR RICE-P","STAGES FOR RICE-K","STAGES FOR SUGARCANE","STAGES FOR SUNFLOWER-RAINFED","STAGES FOR SUNFLOWER-IRRIGATED"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,44 +48,51 @@ public class MainActivity extends AppCompatActivity {
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                seed=plant_stage[0];
+                vegetative=plant_stage[1];
+                flower=plant_stage[2];
+                fruit=plant_stage[3];
+                harvest=plant_stage[4];
                 if(i==0){
 
                    textView.setText(R.string.select);
                 }
                 if(i==1){
-                    stage_name=selectoptions[1];
+                    stage_name=select_options[0];
+
                     imageView.setImageResource(R.drawable.cotton);
                     textView.setText(R.string.cotton_selection);
 
 
                 }
                 if(i==2){
-                    stage_name=selectoptions[2];
+                    stage_name=select_options[1];
                     imageView.setImageResource(R.drawable.rice);
                     textView.setText(R.string.rice_k_selection);
 
                 }
                 if(i==3){
-                    stage_name=selectoptions[3];
+                    stage_name=select_options[2];
                     imageView.setImageResource(R.drawable.rice_1);
                     textView.setText(R.string.rice_p_selection);
 
                 }
                 if(i==4){
-                    stage_name=selectoptions[4];
+                    stage_name=select_options[3];
                     imageView.setImageResource(R.drawable.sugar_cane);
                     textView.setText(R.string.sugarcane_selection);
 
 
                 }
                 if(i==5){
-                    stage_name=selectoptions[5];
+                    stage_name=select_options[4];
                     imageView.setImageResource(R.drawable.sunflower_1);
                     textView.setText(R.string.sunflower_1_selection);
 
                 }
                 if(i==6){
-                    stage_name=selectoptions[6];
+                    stage_name=select_options[5];
                     imageView.setImageResource(R.drawable.sunflower_2);
                     textView.setText(R.string.sunflower_2_selection);
 
@@ -90,13 +102,18 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-        select=stage_name.toUpperCase();
+        /*select=stage_name.toUpperCase();*/
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent;
                 intent=new Intent(MainActivity.this,Stages.class);
-                intent.putExtra("Name",select);
+                intent.putExtra("Name",stage_name);
+                intent.putExtra("seed",seed);
+                intent.putExtra("veg",vegetative);
+                intent.putExtra("flow",flower);
+                intent.putExtra("fru",fruit);
+                intent.putExtra("harv",harvest);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/pesticide/Stages.java
+++ app/src/main/java/com/example/pesticide/Stages.java
@@ -1,23 +1,83 @@
 package com.example.pesticide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class Stages extends AppCompatActivity {
+public class Stages extends AppCompatActivity implements View.OnClickListener {
 
-    TextView t1;
+    TextView t1,t2,t3,t4,t5,t6;
+
+    CardView S_stage,V_stage,F1_stage,F2_stage,H_stage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stages);
 
-        t1=findViewById(R.id.stage_cotton);
+       t1=findViewById(R.id.stage_cotton);
+       t2=findViewById(R.id.seedling);
+       t3=findViewById(R.id.vegetation);
+       t4=findViewById(R.id.flowering);
+       t5=findViewById(R.id.fruiting);
+       t6=findViewById(R.id.harvesting);
 
-        t1.setText(getIntent().getStringExtra("Name"));
+       S_stage=(CardView)findViewById(R.id.card1);
+       V_stage=(CardView)findViewById(R.id.card2);
+       F1_stage=(CardView)findViewById(R.id.card3);
+       F2_stage=(CardView)findViewById(R.id.card4);
+       H_stage=(CardView)findViewById(R.id.card5);
 
 
+        S_stage.setOnClickListener(this);
+        V_stage.setOnClickListener(this);
+        F1_stage.setOnClickListener(this);
+        F2_stage.setOnClickListener(this);
+        H_stage.setOnClickListener(this);
+
+
+       t1.setText(getIntent().getStringExtra("Name"));
+       t2.setText(getIntent().getStringExtra("seed"));
+       t3.setText(getIntent().getStringExtra("veg"));
+       t4.setText(getIntent().getStringExtra("flow"));
+       t5.setText(getIntent().getStringExtra("fru"));
+       t6.setText(getIntent().getStringExtra("harv"));
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.card1:
+                intent=new Intent(Stages.this,Seedling.class);
+                startActivity(intent);
+                break;
+            case R.id.card2:
+                intent=new Intent(Stages.this,Vegetative.class);
+                startActivity(intent);
+                break;
+            case R.id.card3:
+                intent=new Intent(Stages.this,Flowering.class);
+                startActivity(intent);
+                break;
+            case R.id.card4:
+                intent=new Intent(Stages.this,Fruiting.class);
+                startActivity(intent);
+                break;
+            case R.id.card5:
+                intent=new Intent(Stages.this,Harvesting.class);
+                startActivity(intent);
+                break;
+
+    }
     }
 }
