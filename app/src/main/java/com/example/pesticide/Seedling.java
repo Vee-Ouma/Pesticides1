f9b818254b08dc34438842e98d1d8c4309618d94
mckymse <hemhemanth411819.hk@gmail.com>
2019-08-06 18:19:46 +0530

    first update

---
+++ app/src/main/java/com/example/pesticide/Seedling.java
@@ -0,0 +1,13 @@
+package com.example.pesticide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seedling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
